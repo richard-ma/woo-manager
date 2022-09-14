d74bfd1a57174d69c1388067ade23a08aaa59847
richard-ma <richard.ma.19850509@gmail.com>
2022-09-15 05:40:20 +0800

    use wtforms create form

---
+++ app/store/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms import ValidationError
+from wtforms.validators import Length, DataRequired, InputRequired
+
+
+class StoreForm(FlaskForm):
+    url = StringField('Store Url', validators=[DataRequired, Length(0, 128)])
+    key = StringField('Consumer Key', validators=[DataRequired, Length(0, 128)])
+    secret = StringField('Consumer Secret', validators=[DataRequired, Length(0, 128)])
+    version = StringField('API Version', default='wc/v3', validators=[DataRequired, Length(0, 128)])
+    active = BooleanField('Active?', default=True, validators=[InputRequired])
+    submit = SubmitField('Submit')
